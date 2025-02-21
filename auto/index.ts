6bf5271d5ae111efa3b8daa881b262c63ecdaaf7
Okan <okan.unallar@gmail.com>
2025-02-21 20:30:46 +0300

    migration files created

--- auto/index.ts
+++ auto/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250219_074036_add_content_with_image from './20250219_074036_add_content_with_image';
+import * as migration_20250221_172949_v0_0_6 from './20250221_172949_v0_0_6';
 
 export const migrations = [
   {
     up: migration_20250219_074036_add_content_with_image.up,
     down: migration_20250219_074036_add_content_with_image.down,
-    name: '20250219_074036_add_content_with_image'
+    name: '20250219_074036_add_content_with_image',
+  },
+  {
+    up: migration_20250221_172949_v0_0_6.up,
+    down: migration_20250221_172949_v0_0_6.down,
+    name: '20250221_172949_v0_0_6'
   },
 ];
