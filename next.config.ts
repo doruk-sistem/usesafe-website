f29a13dcb494342f9a462ed812893b9a4fca39fa
Okan <okan.unallar@gmail.com>
2025-02-21 20:06:59 +0300

    packages updated

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,26 @@
 import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
-
 import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
+        const url = new URL(item);
+
+        return {
+          hostname: url.hostname,
+          protocol: url.protocol.replace(":", ""),
+        };
+      }),
+    ] as any,
+  },
 };
 
 export default withPayload(withNextIntl(nextConfig));
