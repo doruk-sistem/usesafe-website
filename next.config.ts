2e6653e5d38356e181152f0db009e280524cee7f
alicankocman <kocmanalican@gmail.com>
2025-02-28 10:37:42 +0300

    refactor: update footer with centralized links and SEO improvements

--- next.config.ts
+++ next.config.ts
@@ -9,11 +9,29 @@ const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
   : process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
+      rule.test?.test?.('.svg'),
+    )
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+    )
+    return config
+  },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
         const url = new URL(item);
-
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(":", ""),
