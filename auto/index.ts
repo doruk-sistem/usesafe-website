72d8174754a15e9de09ce5cce97cdbc685e0f3e2
furkanarslan <furkan.arslan@doruksistem.com.tr>
2025-03-07 17:48:58 +0300

    migration fixes

--- auto/index.ts
+++ auto/index.ts
@@ -1,9 +1,6 @@
 import * as migration_20250223_214751 from './20250223_214751';
 import * as migration_20250302_200635 from './20250302_200635';
-import * as migration_20250303_073109_solution_changes from './20250303_073109_solution_changes';
-import * as migration_20250303_085515_solution_changes_2 from './20250303_085515_solution_changes_2';
-import * as migration_20250305_025543_footer_pages from './20250305_025543_footer_pages';
-import * as migration_20250305_040124_blog_pages from './20250305_040124_blog_pages';
+import * as migration_20250307_142520 from './20250307_142520';
 
 export const migrations = [
   {
@@ -17,23 +14,8 @@ export const migrations = [
     name: '20250302_200635',
   },
   {
-    up: migration_20250303_073109_solution_changes.up,
-    down: migration_20250303_073109_solution_changes.down,
-    name: '20250303_073109_solution_changes',
-  },
-  {
-    up: migration_20250303_085515_solution_changes_2.up,
-    down: migration_20250303_085515_solution_changes_2.down,
-    name: '20250303_085515_solution_changes_2',
-  },
-  {
-    up: migration_20250305_025543_footer_pages.up,
-    down: migration_20250305_025543_footer_pages.down,
-    name: '20250305_025543_footer_pages',
-  },
-  {
-    up: migration_20250305_040124_blog_pages.up,
-    down: migration_20250305_040124_blog_pages.down,
-    name: '20250305_040124_blog_pages'
+    up: migration_20250307_142520.up,
+    down: migration_20250307_142520.down,
+    name: '20250307_142520'
   },
 ];
