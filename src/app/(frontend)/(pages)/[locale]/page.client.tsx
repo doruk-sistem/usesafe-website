56e1064881647bae587235184479deab9deaf274
alicankocman <kocmanalican@gmail.com>
2025-08-25 10:08:09 +0300

    feat: replace homepage with usesafe-certification page content

--- src/app/(frontend)/(pages)/[locale]/page.client.tsx
+++ src/app/(frontend)/(pages)/[locale]/page.client.tsx
@@ -1,20 +1,7 @@
 "use client";
 
-import React from "react";
+import UseSafeCertificationPage from "./platform/products/usesafe-certification/page";
 
-import NewsletterBlock from "@/blocks/newsletter-block";
-import RenderBlocks from "@/blocks/RenderBlocks";
-import { Config } from "@/payload-types";
-
-interface PageClientProps {
-  layout: Config["globals"]["homepage"]["layout"];
-}
-
-export default function PageClient({ layout }: PageClientProps) {
-  return (
-    <div>
-      <RenderBlocks blocks={(layout || []) as any} />
-      <NewsletterBlock />
-    </div>
-  );
+export default function PageClient() {
+  return <UseSafeCertificationPage />;
 }
